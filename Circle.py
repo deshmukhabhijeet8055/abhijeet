b0b390302b15238743679914c41d682c3569c799
Abhijeet Deshmukh <abhijed1@amdocs.com>
2018-08-27 18:59:12 +0530

    new comment added

--- Circle.py
+++ Circle.py
@@ -1,5 +1,6 @@
 #program to find area and circumference of circle
 #added comment in the script
+#new comment
 from math import pi
 radius = float(input("Enter radius of a circle"))
 print("Area of circle = " + str(pi * radius**2))
