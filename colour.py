bcf3c317c0d63e26293884f75dbefb20e3e82af4
deshmukhabhijeet8055 <42742164+deshmukhabhijeet8055@users.noreply.github.com>
2018-08-27 19:43:29 +0530

    Update colour.py

--- colour.py
+++ colour.py
@@ -1,4 +1,5 @@
 #program to find out the first and last number the list
+#changing this for git push
 colour_list = [12, 14, 15, 26, 52, 34]
 for each_colour in colour_list:
 	print("Colour number is " + str(each_colour))
