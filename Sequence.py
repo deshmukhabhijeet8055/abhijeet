f6c74431a191e5405eacc6e37599d559b4403c92
Abhijeet Deshmukh <abhijed1@amdocs.com>
2018-08-27 19:45:47 +0530

    comment added

--- Sequence.py
+++ Sequence.py
@@ -1,4 +1,5 @@
 #program to take values from user and turn them into a list and tuple
+#comment added
 values = str(input("Enter some comma seperated values"))
 my_list = values.split(",")
 my_tuple = tuple(my_list)
