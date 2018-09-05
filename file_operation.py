be02b29b025794e05e031e0f6b5de6154042d127
Abhijeet Deshmukh <abhijed1@amdocs.com>
2018-09-05 15:15:53 +0530

    Manual change in file

--- file_operation.py
+++ file_operation.py
@@ -1,1 +1,10 @@
-#program to understand file operation
+#program to understand file operations
+
+with open('newfile.txt', mode = 'w') as f:
+	f.write("This is the heading of this file.")
+	f.write("This is second line")
+	f.write("This is third line")
+
+with open('newfile.txt', mode = 'r') as f:
+	for word in f.readlines():
+		print(word + "\n")
