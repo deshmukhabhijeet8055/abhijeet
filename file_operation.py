f4c8dcbf89b7cce41ca4f0b01414c8f4e3c5e56a
Abhijeet Deshmukh <abhijed1@amdocs.com>
2018-09-05 15:26:44 +0530

    file operation changes

--- file_operation.py
+++ file_operation.py
@@ -1,10 +1,16 @@
 #program to understand file operations
 
 with open('newfile.txt', mode = 'w') as f:
-	f.write("This is the heading of this file.")
-	f.write("This is second line")
-	f.write("This is third line")
+	f.write("This is the heading of this file.\n")
+	f.write("This is second line.\n")
+	f.write("This is third line.\n")
 
 with open('newfile.txt', mode = 'r') as f:
 	for word in f.readlines():
-		print(word + "\n")
+		print(word)
+
+print("Second line of the file is")
+with open('newfile.txt', mode = 'r') as f:
+	for num_word in enumerate(f.readlines()):
+		if num_word[0] == 1:
+			print(num_word[1])
