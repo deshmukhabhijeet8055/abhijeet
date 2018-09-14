210d183bdfc0aee60790327d5898d03fef6883be
Abhijeet Deshmukh <abhijed1@amdocs.com>
2018-09-14 18:21:00 +0530

    new exception python added

--- Python Codes/exception_1.py
+++ Python Codes/exception_1.py
@@ -1,1 +1,17 @@
 #Errors and exception handling
+#FileNotFoundException
+try:
+    '''
+    This is a DOCSTRING for this try block
+    '''
+    with open('test_xml.xml', 'r') as f:
+        FILE_ROWS = f.readlines()
+        for EACH_ROW in FILE_ROWS:
+            print(EACH_ROW)
+except FileNotFoundError:
+    print("Mentioned file doesn't exist")
+else:
+    print('File Contents Ends here')
+finally:
+    print('Read file contents program ends here')
+
